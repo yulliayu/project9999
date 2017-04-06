7417aa792c93618acaba8bfaa07ce91fafa36c67
user-pc\user <seize810@gmail.com>
2017-04-06 18:27:31 +0900

    ver 1.2 (20170406 1827)

--- src/echo/server/MyEchoServer.java
+++ src/echo/server/MyEchoServer.java
@@ -1,5 +1,10 @@
 package echo.server;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,9 +12,20 @@ public class MyEchoServer {
 	
 	ServerSocket  server;
 	int port = 5555;
+	Socket  socket;
 	
 	public MyEchoServer() {
 		
+		try {
+			server = new ServerSocket(port);
+			socket = server.accept();
+			
+			BufferedReader buffr= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			BufferedWriter buffw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
